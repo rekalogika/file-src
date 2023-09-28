4301ead77a38543953a2a19ee52c9c1da6ec65ee
Priyadi Iman Nurcahyo <1102197+priyadi@users.noreply.github.com>
2023-09-28 21:33:06 +0700

    doctrine config

--- packages/file-bundle/src/RekalogikaFileBundle.php
+++ packages/file-bundle/src/RekalogikaFileBundle.php
@@ -13,6 +13,8 @@ declare(strict_types=1);
 
 namespace Rekalogika\File\Bundle;
 
+use Doctrine\Bundle\DoctrineBundle\DependencyInjection\Compiler\DoctrineOrmMappingsPass;
+use Rekalogika\Domain\File\Association\Entity\EmbeddedMetadata;
 use Rekalogika\File\Bundle\DependencyInjection\FilterPass;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
@@ -28,7 +30,33 @@ class RekalogikaFileBundle extends Bundle
     {
         parent::build($container);
 
-        $container
-            ->addCompilerPass(new FilterPass());
+        // filter
+        $container->addCompilerPass(new FilterPass());
+
+        // doctrine
+        if (
+            class_exists(EmbeddedMetadata::class)
+            && class_exists(DoctrineOrmMappingsPass::class)
+        ) {
+            $path = (new \ReflectionClass(EmbeddedMetadata::class))
+                ->getFileName();
+
+            if (empty($path)) {
+                throw new \RuntimeException('Unable to get path of EmbeddedMetadata class');
+            }
+
+            $configDir = realpath(dirname(dirname($path)) . 'config/doctrine');
+            if (false === $configDir) {
+                throw new \RuntimeException('Unable to get path of EmbeddedMetadata class');
+            }
+
+            $mappings = [
+                $configDir => 'Rekalogika\Domain\File\Association\Entity',
+            ];
+
+            $container->addCompilerPass(
+                DoctrineOrmMappingsPass::createXmlMappingDriver($mappings)
+            );
+        }
     }
 }
