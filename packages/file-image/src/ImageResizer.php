d4938184070c49eedaf848010a39a002e3954eff
Priyadi Iman Nurcahyo <1102197+priyadi@users.noreply.github.com>
2024-02-15 13:08:36 +0700

    fix: encode()

--- packages/file-image/src/ImageResizer.php
+++ packages/file-image/src/ImageResizer.php
@@ -13,6 +13,7 @@ declare(strict_types=1);
 
 namespace Rekalogika\File\Image;
 
+use Intervention\Image\Encoders\AutoEncoder;
 use Intervention\Image\ImageManager;
 use Rekalogika\Contracts\File\FileInterface;
 use Rekalogika\File\Derivation\Filter\AbstractFileFilter;
@@ -98,7 +99,7 @@ class ImageResizer extends AbstractFileFilter
         }
 
         $img->resize((int) round($width), (int) round($height));
-        $img->encode();
+        $encoded = $img->encode(new AutoEncoder);
 
         return $this->getFileRepository()
             ->createFromString(
