f4faa2729775496178af29e8dcfc09f7e20e508c
Priyadi Iman Nurcahyo <1102197+priyadi@users.noreply.github.com>
2024-06-10 13:48:22 +0700

    clean up

--- packages/file-metadata/src/Metadata/HttpMetadata.php
+++ packages/file-metadata/src/Metadata/HttpMetadata.php
@@ -112,9 +112,14 @@ final class HttpMetadata extends AbstractMetadata implements
     private function getLastModified(): ?string
     {
         $lastModified = $this->metadata->tryGet(Constants::FILE_MODIFICATION_TIME);
-        $lastModified = (new \DateTimeImmutable())->setTimestamp($lastModified);
 
-        return $lastModified === false ? null : $lastModified->format(\DateTimeInterface::RFC7231);
+        if ($lastModified === null || $lastModified === false) {
+            return null;
+        }
+
+        return (new \DateTimeImmutable())
+            ->setTimestamp((int) $lastModified)
+            ->format(\DateTimeInterface::RFC7231);
     }
 
     public function getETag(): ?string
