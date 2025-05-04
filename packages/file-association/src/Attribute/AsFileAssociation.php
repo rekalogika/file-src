bfeff6ba9d54f2ad8c99a4dc5dab6e742914014d
Priyadi Iman Nurcahyo <1102197+priyadi@users.noreply.github.com>
2025-05-04 12:33:35 +0700

    deprecation: stop accepting string as `AsFileAssociation` argument (#123)

--- packages/file-association/src/Attribute/AsFileAssociation.php
+++ packages/file-association/src/Attribute/AsFileAssociation.php
@@ -18,30 +18,7 @@ use Rekalogika\File\Association\Model\FetchMode;
 #[\Attribute(\Attribute::TARGET_PROPERTY)]
 final readonly class AsFileAssociation
 {
-    public FetchMode $fetch;
-
-    /**
-     * @param 'EAGER'|'LAZY'|FetchMode $fetch
-     */
     public function __construct(
-        FetchMode|string $fetch = FetchMode::Eager,
-    ) {
-        if (\is_string($fetch)) {
-            trigger_deprecation(
-                package: 'rekalogika/file-association',
-                version: '2.0.0',
-                message: 'Passing a string as the first argument to "%s" is deprecated, use the FetchMode enum instead.',
-                args: __CLASS__,
-            );
-
-            $fetch = match (strtoupper($fetch)) {
-                'EAGER' => FetchMode::Eager,
-                // @phpstan-ignore match.alwaysTrue
-                'LAZY' => FetchMode::Lazy,
-                default => throw new \InvalidArgumentException(\sprintf('Invalid fetch mode "%s".', $fetch)),
-            };
-        }
-
-        $this->fetch = $fetch;
-    }
+        public FetchMode $fetch = FetchMode::Eager,
+    ) {}
 }
